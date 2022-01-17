95566ab5aa1705a19e1ad8e58bd929c28c3b3660
Sophie Bartmann <sophie.e.bartmann@gmail.com>
2022-01-17 19:28:12 +0100

    Added a few snacks.

--- snacks.py
+++ snacks.py
@@ -1,4 +1,5 @@
 snacks=[ 'eine handvoll Salzbrezeln','ein Schälchen Chips','5 M&Ms','ein Päckchen gesalzene Nüsse',
          'ein paar Apfelschnitze','eine Tüte salziges Popcorn','ein Täfelchen Schokolade','ein kleines Stück Käsekuchen',
          'ein kleines Stück Kuchen nach Wahl','ein kleines Schälchen Schokoladen Keksi', 'eine Hand voll Trockenfrüchte',
-         'ein gesunder Riegel','eine Tüte Luft']
+         'ein gesunder Riegel','eine Tüte Luft', '5 Schokobons', 'eine Schale Nachos','einen kinderriegel',
+         'eine schwarze Lindor Schokokugel']
