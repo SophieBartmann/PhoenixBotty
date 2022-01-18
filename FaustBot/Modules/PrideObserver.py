1092d869c0609c86f52f01cd742d43b8c2d9cf59
Sophie Bartmann <sophie.e.bartmann@gmail.com>
2022-01-18 13:43:29 +0100

    Minor spelling correction.

--- FaustBot/Modules/PrideObserver.py
+++ FaustBot/Modules/PrideObserver.py
@@ -9,9 +9,9 @@ class PrideObserver(PrivMsgObserverPrototype):
 
     @staticmethod
     def help():
-        return ".party - sorgt für Pride Flags"
+        return ".party - sorgt für sehr viele Pride Flags"
 
     def update_on_priv_msg(self, data: dict, connection: Connection):
         if data['message'].find('.pride') == -1:
             return
-        connection.send_back('\001ACTION schmückt den Channel mit vielen großen Pride flags.\001', data)
+        connection.send_back('\001ACTION schmückt den Channel mit ganz vielen großen Pride Flaggen.\001', data)
